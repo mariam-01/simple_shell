81b86bc8a1f04ed389ec4a4f6d64cbd65c58f009
mariam-01 <mariam.larhlid2001@gmail.com>
2023-06-27 07:42:34 -0700

    d

--- path.c
+++ path.c
@@ -69,24 +69,5 @@ int _PATHstrcmp(const char *s1, const char *s2)
 			return (-1);
 	}
 	return (0);
-}
-/**
- * _concat - concats user string with PATH member string and /
- * @tmp: static array to store concatenated string
- * @av: pointer to array of user strings
- * @tok: pointer to PATH token
- *
- * Return: 0 on success
- */
-char *_concat(char *tmp, char **av, char *tok)
-{
-	int len = 0;
 
-	_memset(tmp, 0, 256);
-	len = _strlen(tok) + _strlen(av[0]) + 2;
-	_strcat(tmp, tok);
-	_strcat(tmp, "/");
-	_strcat(tmp, av[0]);
-	tmp[len - 1] = '\0';
-	return (tmp);
 }
